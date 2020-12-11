f42924e2aa813b9255b12a03e5a8a72d32527b35
shojikenta <shojikenta4@gmail.com>
2020-12-11 20:00:26 +0900

    basic login page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  include SessionsHelper
+  
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "メールアドレスか、パスワードが間違ってます😢"
+      render 'new'
+    end
+  end
+end
